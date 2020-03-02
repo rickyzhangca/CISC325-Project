2c04e87c2a7ef85c798936ea6485e3517ea159ab
Ricky Zhang <ricky.zhang@queensu.ca>
2020-03-02 12:07:18 -0500

    Initialize

---
+++ housemate/housemate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  housemate
+//
+//  Created by Ricky Zhang on 2020-03-02.
+//  Copyright © 2020 CISC325 Group 10. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
